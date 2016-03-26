3bfc1c213f95c7ac8613292e92747fb83c505cdb
Pedro Medeiros <pedrosnk@gmail.com>
2016-03-26 18:54:24 -0300

    starts with gin

---
+++ gin/lib/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "WORKING",
+		})
+	})
+
+	r.Run(":8888")
+}
