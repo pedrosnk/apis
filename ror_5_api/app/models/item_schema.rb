ca6802554baef469dd92f17c391d285f27a4aa64
Pedro Medeiros <pedrosnk@gmail.com>
2016-05-08 15:41:39 -0300

    validaties convertion to json_schema

--- ror_5_api/app/models/item_schema.rb
+++ ror_5_api/app/models/item_schema.rb
@@ -1,3 +1,4 @@
+require 'json-schema'
 class ItemSchema
   include Mongoid::Document
   validate :fully_validate_item_schema
@@ -5,13 +6,26 @@ class ItemSchema
   field :properties, type: Hash
   field :schema, type: String
   field :type, type: String
-  field :aditionalProperties, type: Mongoid::Boolean
+  field :aditional_properties, type: Mongoid::Boolean
   field :description, type: String
   field :collection_name, type: String
   field :required, type: Array
 
+  def to_json_schema
+    schema = attributes.stringify_keys
+    schema['$schema'] = schema.delete 'schema'
+    schema['aditionalProperties'] = schema.delete 'aditional_properties'
+    schema['collectionName'] = schema.delete 'collection_name'
+    schema['id'] = schema.delete('_id').to_s
+    schema
+  end
+
   private
   def fully_validate_item_schema
-    JSON::Validator.fully_validate_schema(attributes).empty?
+    schema = attributes.stringify_keys
+    if JSON::Validator.fully_validate_schema(schema).empty?
+
+    end
   end
+
 end
--- ror_5_api/spec/models/item_schema_spec.rb
+++ ror_5_api/spec/models/item_schema_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe ItemSchema, type: :model do
     it { expect(subject).to be_valid }
   end
 
+  describe '#to_json_schema' do
+    subject { FactoryGirl.build :item_schema }
+
+    let :json_schema do
+      { 
+        '$schema' => subject[:schema],
+        'aditionalProperties' => subject[:aditional_properties],
+        'collectionName' => subject[:collection_name],
+        'type' => subject[:type],
+        'required' => subject[:required],
+        'properties' => subject[:properties],
+        'description' => subject[:description],
+        'id' => subject[:_id].to_s,
+      }
+    end
+
+    it { expect(subject.to_json_schema).to eql(json_schema) }
+  end
+
 end
